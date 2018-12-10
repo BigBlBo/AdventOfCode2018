7f0f67a5ccd2bfbe9249d35b4422e3cffc3a1cdd
Blaz Bozjak <blaz.bozjak@gmail.com>
2018-12-10 07:24:11 +0100

    DAy 10 init

--- AdventOfCode2018/Run.cs
+++ AdventOfCode2018/Run.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode2018
         static void Main(string[] args)
         {
             Stopwatch sw = Stopwatch.StartNew();
-            /*
+            
             Day1 day1 = new Day1(); day1.Part1(); day1.Part2();
             Day2 day2 = new Day2(); day2.Part1(); day2.Part2();
             Day3 day3 = new Day3(); day3.Part1(); day3.Part2();
@@ -20,10 +20,10 @@ namespace AdventOfCode2018
             Day7 day7 = new Day7(); day7.Part1(); day7.Part2();
             
             Day8 day8 = new Day8(); day8.Part1(); day8.Part2();
-            */
-            Day9 day9 = new Day9();
-            day9.Part1(); Console.WriteLine(sw.ElapsedMilliseconds);
-            day9.Part2();
+            
+            Day9 day9 = new Day9(); day9.Part1(); day9.Part2();
+
+            Day10 day10 = new Day10(); day10.Part1(); day10.Part2();
 
             Console.WriteLine(sw.ElapsedMilliseconds);
             Console.ReadLine();
