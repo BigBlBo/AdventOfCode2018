4b58fa08733f7982f52a196204b2d956ba5082a4
Blaz Bozjak <blaz.bozjak@gmail.com>
2018-12-12 19:37:24 +0100

    small optimization

--- AdventOfCode2018/Days/Day12.cs
+++ AdventOfCode2018/Days/Day12.cs
@@ -9,26 +9,26 @@ namespace AdventOfCode2018.Days
     {
         public void Part1()
         {
-            Console.WriteLine("End result of day  8 (part 1) is " + GrowPlants(20, false));
+            Console.WriteLine("End result of day 12 (part 1) is " + GrowPlants(20, false));
         }
 
         public void Part2()
         {
-            Console.WriteLine("End result of day  8 (part 2) is " + GrowPlants(50000000000L, true));
+            Console.WriteLine("End result of day 12 (part 2) is " + GrowPlants(50000000000L, true));
         }
 
         private long GrowPlants(long generations, bool stopWhenRepeated)
         {
             string curentGeneration = "#.#..#..###.###.#..###.#####...########.#...#####...##.#....#.####.#.#..#..#.#..###...#..#.#....##.";
             List<PlantGrowPattern> plantGrowPatterns = new FileRead().GetPlantGrowPatterns("../../../Inputs/Day12.txt");
-            string addToNewGeneration = ".....";
+            string addToNewGeneration = "...";
             long sumPrev = 0;
-            long sum = CountPlantPots(curentGeneration.ToCharArray(), 0);
+            long sum = CountPlantPots(curentGeneration.ToCharArray(), 0, addToNewGeneration);
             long prevNowDiff = 0;
 
             for (long index = 1; index <= generations; index++)
             { 
-                char[] charArrayNewGeneration = new String('.', curentGeneration.Length + 10).ToCharArray();
+                char[] charArrayNewGeneration = new String('.', curentGeneration.Length + 6).ToCharArray();
                 char[] charArrayCurentGeneration = curentGeneration.
                         Insert(curentGeneration.Length-1, addToNewGeneration).
                             Insert(0, addToNewGeneration).ToCharArray();
@@ -55,7 +55,7 @@ namespace AdventOfCode2018.Days
                     }
                 }
 
-                sum = CountPlantPots(charArrayNewGeneration, index);
+                sum = CountPlantPots(charArrayNewGeneration, index, addToNewGeneration);
                 curentGeneration = new string(charArrayNewGeneration);
                 if (stopWhenRepeated && prevNowDiff == (sum - sumPrev))
                 {
@@ -69,10 +69,10 @@ namespace AdventOfCode2018.Days
             return sum;
         }
 
-        private long CountPlantPots(char[] curentGeneration, long generationInProgress)
+        private long CountPlantPots(char[] curentGeneration, long generationInProgress, string addToNewGeneration)
         {
             long sum = 0;
-            long untilZeroPosition = generationInProgress * 5;
+            long untilZeroPosition = generationInProgress * addToNewGeneration.Length;
             for (int index = 0; index < curentGeneration.Length; index++)
             {
                 if (curentGeneration[index] == '#')
--- AdventOfCode2018/Run.cs
+++ AdventOfCode2018/Run.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode2018
         static void Main(string[] args)
         {
             Stopwatch sw = Stopwatch.StartNew();
-            /*
+            
             Day01 day01 = new Day01(); day01.Part1(); day01.Part2();
             Day02 day02 = new Day02(); day02.Part1(); day02.Part2();
             Day03 day03 = new Day03(); day03.Part1(); day03.Part2();
@@ -20,7 +20,7 @@ namespace AdventOfCode2018
             Day08 day08 = new Day08(); day08.Part1(); day08.Part2();
             Day09 day09 = new Day09(); day09.Part1(); day09.Part2();
             Day10 day10 = new Day10(); day10.Part1(); day10.Part2();
-            Day11 day11 = new Day11(); day11.Part1(); day11.Part2();*/
+            //Day11 day11 = new Day11(); day11.Part1(); day11.Part2();
             Day12 day12 = new Day12(); day12.Part1(); day12.Part2();
 
             Console.WriteLine(sw.ElapsedMilliseconds);
